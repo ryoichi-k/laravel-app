b7f6954c18fcac6737391fa81bc3ed8daec065a7
ryoichi-k <kazyuapple99@gmail.com>
2022-01-19 14:32:25 +0900

    日本語対応 validation.php追加

--- laravel/resources/views/Auth/register.blade.php
+++ laravel/resources/views/Auth/register.blade.php
@@ -10,6 +10,8 @@
         <div class="card mt-3">
           <div class="card-body text-center">
             <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
+            
+            @include('error_card_list')
 
             <div class="card-text">
               {{--ここから--}}
